12bbd73c3dd3c9f6508a21874fc4fcb053ccc57f
Suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2025-10-24 21:55:54 +0530

    Add MongoDB configuration settings

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+MONGO_URI = "mongodb+srv://suhassambargi021:Suhas_0s7@cluster0.4e2b0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+DB_NAME = "numberplates_db"
+COLLECTION_NAME = "plates"
